af69fd48514c9a594dfcb32cb5ae9a4718b4d70c
steve2482 <stevecb12684@yahoo.com>
2016-07-18 15:26:12 -0400

    Finished Project

--- js/app.js
+++ js/app.js
@@ -2,32 +2,46 @@ $(document).ready(function() {
 	$(".ryu").mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-  	})
-  	.mouseleave(function() {
+	})
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-  	})
-  	.mousedown(function() {
-  		playHadouken();
-  		$('.ryu-ready').hide();
-  		$('.ryu-throwing').show();
-  		// play hadouken sound
-  		$('.hadouken').finish().show()
-  	   		.animate(
-  	   		{'left': '1020px'},
-  	   		500,
-  	   		function() {
-  	   			$(this).hide();
-  	   			$(this).css('left', '520px');
-  	   		}
-  		);
-  	})
-  	.mouseup(function() {
-  		$('.ryu-throwing').hide();
-  		$('.ryu-ready').show();
-  	})
-});
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+   		.animate(
+   		{'left': '1020px'},
+   		500,
+   		function() {
+   			$(this).hide();
+   			$(this).css('left', '520px');
+   		}
+		);
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
 
+  	$(document).on("keydown", function(event) {
+  		console.log( event.which, event.which == 88);
+  		if ( event.which == 88) {
+  			$('.ryu-still').hide();
+  			$('.ryu-cool').show();
+  		}
+
+	})
+  	.on("keyup", function(event) {
+  		if (event.which == 88) {
+  			$('.ryu-cool').hide();
+  			$('.ryu-still').show();
+  		}
+  	});
+
+  	});
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
